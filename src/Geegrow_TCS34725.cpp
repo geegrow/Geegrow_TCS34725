de349b4de88a7baaccdc1c5faea415695cadafcf
kaktus <info@geegrow.ru>
2019-04-23 17:45:34 +0300

    implemented calibration for 255 format

--- src/Geegrow_TCS34725.cpp
+++ src/Geegrow_TCS34725.cpp
@@ -61,7 +61,7 @@ void Geegrow_TCS34725::disable() {
     @note     There is a delay in this function for getting a valid RGBC value
  */
 /******************************************************************************/
-void Geegrow_TCS34725::getRawData(uint16_t &red, uint16_t &green, uint16_t &blue, uint16_t &clear) {
+void Geegrow_TCS34725::getRawData(int16_t &red, int16_t &green, int16_t &blue, int16_t &clear) {
     delay(this->currentIntegrationTime);
     red   = this->I2C_read_16(RN_RDATAL);
     green = this->I2C_read_16(RN_GDATAL);
@@ -77,8 +77,8 @@ void Geegrow_TCS34725::getRawData(uint16_t &red, uint16_t &green, uint16_t &blue
     @param    blue    Reference to variable for blue color
  */
 /******************************************************************************/
-void Geegrow_TCS34725::getRGB_255(uint16_t &red, uint16_t &green, uint16_t &blue) {
-    uint16_t temp_red, temp_green, temp_blue, temp_clear;
+void Geegrow_TCS34725::getRGB_255(int16_t &red, int16_t &green, int16_t &blue) {
+    int16_t temp_red, temp_green, temp_blue, temp_clear;
     this->getRawData(temp_red, temp_green, temp_blue, temp_clear);
 
     if (temp_clear == 0) {
@@ -86,9 +86,93 @@ void Geegrow_TCS34725::getRGB_255(uint16_t &red, uint16_t &green, uint16_t &blue
         return;
     }
 
-    red = (uint16_t)((float)temp_red / (float)temp_clear * 255.0);
-    green = (uint16_t)((float)temp_green / (float)temp_clear * 255.0);
-    blue = (uint16_t)((float)temp_blue / (float)temp_clear * 255.0);
+    temp_red   -= this->calibrationDiff[0];
+    temp_green -= this->calibrationDiff[1];
+    temp_blue  -= this->calibrationDiff[2];
+
+    red   = (int16_t)((float)temp_red   / (float)(this->calibrationAvg) * 255.0);
+    green = (int16_t)((float)temp_green / (float)(this->calibrationAvg) * 255.0);
+    blue  = (int16_t)((float)temp_blue  / (float)(this->calibrationAvg) * 255.0);
+
+    if (red > 255) red = 255;
+    if (green > 255) green = 255;
+    if (blue > 255) blue = 255;
+
+    if (red < 0) red = 0;
+    if (green < 0) green = 0;
+    if (blue < 0) blue = 0;
+}
+
+/******************************************************************************/
+/*!
+    @brief    Enables interrupts on RGBC limit values
+ */
+/******************************************************************************/
+void Geegrow_TCS34725::enableIRQ() {
+    uint8_t t = this->I2C_read_8(RN_ENABLE);
+    this->I2C_write_8(RN_ENABLE, t | RN_ENABLE_AIEN);
+}
+
+/******************************************************************************/
+/*!
+    @brief    Disables interrupts on RGBC limit values
+ */
+/******************************************************************************/
+void Geegrow_TCS34725::disableIRQ() {
+    Wire.beginTransmission(this->i2c_addr);
+    Wire.write(RN_COMMAND_CMD | RN_COMMAND_TYPE_SF | RN_COMMAND_ADDRSF_CLR_IRQ);
+    Wire.endTransmission();
+}
+
+/******************************************************************************/
+/*!
+    @brief    Sets values of IRQ limits
+    @param    high    Higher limit
+    @param    low     Lower limit
+ */
+/******************************************************************************/
+void Geegrow_TCS34725::setLimitsIRQ(int16_t high, int16_t low) {
+    this->I2C_write_8(RN_AIHTH, high >> 8);
+    this->I2C_write_8(RN_AIHTL, high & 0xFF);
+    this->I2C_write_8(RN_AILTH, low >> 8);
+    this->I2C_write_8(RN_AILTL, low & 0xFF);
+}
+
+/******************************************************************************/
+/*!
+    @brief    Realize calibration of the sensor
+ */
+/******************************************************************************/
+void Geegrow_TCS34725::calibrate() {
+int32_t RGB_sums[] = {0, 0, 0};
+    int16_t red, green, blue, clear;
+    const uint8_t maxSamplesNum = 30;
+    /* Calc number of samples */
+    uint8_t samplesNum = 5000 / this->currentIntegrationTime;
+    if (samplesNum > maxSamplesNum)
+        samplesNum = maxSamplesNum;
+
+    /**********************    Take white value    ****************************/
+    Serial.println("Calibration starts. Bring a white standard to the sensor in 5 sec");
+    delay(5000);
+    Serial.println("Calibrating..");
+
+    for (uint8_t i = 0; i < samplesNum; i++) {
+        this->getRawData(red, green, blue, clear);
+        RGB_sums[0] += red;
+        RGB_sums[1] += green;
+        RGB_sums[2] += blue;
+    }
+
+    for (uint8_t i = 0; i < 3; i++) {
+        RGB_sums[i] = RGB_sums[i] / samplesNum;
+    }
+
+    this->calibrationAvg = (RGB_sums[0] + RGB_sums[1] + RGB_sums[2]) / 3;
+    for (uint8_t i = 0; i < 3; i++)
+        this->calibrationDiff[i] = RGB_sums[i] - this->calibrationAvg;
+
+    Serial.println("Calibration finished. Sensor is ready.");
 }
 
 /******************************************************************************/
@@ -127,41 +211,6 @@ void Geegrow_TCS34725::setIntegrationTime(uint8_t time) {
 void Geegrow_TCS34725::setGain(uint8_t gain) {
     this->I2C_write_8(RN_CONTROL, gain);
     this->currentGain = gain;
-}
-
-/******************************************************************************/
-/*!
-    @brief    Enables interrupts on RGBC limit values
- */
-/******************************************************************************/
-void Geegrow_TCS34725::enableIRQ() {
-    uint8_t t = this->I2C_read_8(RN_ENABLE);
-    this->I2C_write_8(RN_ENABLE, t | RN_ENABLE_AIEN);
-}
-
-/******************************************************************************/
-/*!
-    @brief    Disables interrupts on RGBC limit values
- */
-/******************************************************************************/
-void Geegrow_TCS34725::disableIRQ() {
-    Wire.beginTransmission(this->i2c_addr);
-    Wire.write(RN_COMMAND_CMD | RN_COMMAND_TYPE_SF | RN_COMMAND_ADDRSF_CLR_IRQ);
-    Wire.endTransmission();
-}
-
-/******************************************************************************/
-/*!
-    @brief    Sets values of IRQ limits
-    @param    high    Higher limit
-    @param    low     Lower limit
- */
-/******************************************************************************/
-void Geegrow_TCS34725::setLimitsIRQ(uint16_t high, uint16_t low) {
-    this->I2C_write_8(RN_AIHTH, high >> 8);
-    this->I2C_write_8(RN_AIHTL, high & 0xFF);
-    this->I2C_write_8(RN_AILTH, low >> 8);
-    this->I2C_write_8(RN_AILTL, low & 0xFF);
 }
 
 /******************************************************************************/
--- src/Geegrow_TCS34725.h
+++ src/Geegrow_TCS34725.h
@@ -35,20 +35,21 @@ class Geegrow_TCS34725 {
     public:
         Geegrow_TCS34725(
             uint8_t i2c_addr = TCS34725_I2C_ADDRESS,
-            uint8_t time = RN_ATIME_INTEG_TIME_700,
+            uint8_t time = RN_ATIME_INTEG_TIME_154,
             uint8_t gain = RN_CONTROL_GAIN_1X
         );
         void enable();
         void disable();
-        void getRawData(uint16_t &red, uint16_t &green, uint16_t &blue, uint16_t &clear);
-        void getRGB_255(uint16_t &red, uint16_t &green, uint16_t &blue);
-        void setIntegrationTime(uint8_t integrationTime);
-        void setGain(uint8_t gain);
+        void getRawData(int16_t &red, int16_t &green, int16_t &blue, int16_t &clear);
+        void getRGB_255(int16_t &red, int16_t &green, int16_t &blue);
         void enableIRQ();
         void disableIRQ();
-        void setLimitsIRQ(uint16_t high, uint16_t low);
-
+        void setLimitsIRQ(int16_t high, int16_t low);
+        void calibrate();
+        
     private:
+        void setIntegrationTime(uint8_t integrationTime);
+        void setGain(uint8_t gain);
         void I2C_write_8(uint8_t reg, uint8_t value);
         uint8_t I2C_read_8(uint8_t reg);
         uint16_t I2C_read_16(uint8_t reg);
@@ -56,6 +57,8 @@ class Geegrow_TCS34725 {
         uint16_t currentIntegrationTime = 0;
         uint8_t currentGain = 0;
         uint8_t i2c_addr = 0;
+        int16_t calibrationAvg = 0;
+        int16_t calibrationDiff[3];
 };
 
 #endif /* GEEGROW_TCS34725_H */
